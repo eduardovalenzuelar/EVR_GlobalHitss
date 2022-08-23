92bad161a9796696d185be7448c85310e729085e
Eduardo Valenzuela <eduardovalenzuelar@gmail.com>
2022-08-23 12:56:23 -0500

    Validaciones

--- CapaDatos/Models/CLIENTES.cs
+++ CapaDatos/Models/CLIENTES.cs
@@ -18,16 +18,25 @@ namespace CapaDatos.Models
         public long dbid { get; set; }
 
         [Display (Name ="Tipo de Identificacion")]
+        [Required]
         public long dbidTipoIdentificacion { get; set; }
-
+        [Required]
+        [RegularExpression("^[a-zA-Z ]+$")]
         public string Nombre { get; set; }
-
+        
         [Display(Name = "Numero Identificacion")]
+        [Required]
         public string Numero_identificacion { get; set; }
+        [Required]
+        [Display(Name = "Correo Electronico")]
+        [EmailAddress]
         public string email { get; set; }
+        [Required]
         public long Movil { get; set; }
+        [Required]
         public string Direccion { get; set; }
         [Display(Name = "Direccion de Instalacion")]
+        [Required]
         public string Direccion_Instalacion { get; set; }
         public Nullable<System.DateTime> Fechacreacion { get; set; }
         public Nullable<System.DateTime> FechaModificacion { get; set; }
@@ -35,6 +44,7 @@ namespace CapaDatos.Models
         public int Estado { get; set; }
 
         [Display(Name = "Tipo de Identificacion")]
+        [Required]
         public virtual TIPO_IDENTIFICACION TIPO_IDENTIFICACION { get; set; }
     }
 }
